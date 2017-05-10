cbe155cce1ed2305cbda0b5971f1bb65af9dd057
John Cwikla <cwikla@fwix.com>
2017-05-10 13:25:30 -0700

    More 5.0ness

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,19 @@
 # Load the Rails application.
 require_relative 'application'
 
+APP_NAME = "Bullseye"
+TAG_LINE = "Sourcing the World"
+COMPANY_NAME = "Bullseye"
+
+EMAIL_DOMAIN = "cwikla.com"
+DEFAULT_APP_MAILER_ADDR = "info@#{EMAIL_DOMAIN}"
+EXCEPTION_NOTIFICATION_EMAIL = "cwikla@#{EMAIL_DOMAIN}"
+
+INTERNAL_NOTIFICATION_EMAIL = "cwikla@#{EMAIL_DOMAIN}"
+
+ASSET_BUCKET_NAME = "assets"
+
+USERNAME_REGEX = '[a-zA-Z0-9_]*[a-zA-Z][a-zA-Z0-9_]*'
+
 # Initialize the Rails application.
 Rails.application.initialize!
---
+++ config/initializers/00_init.rb
@@ -0,0 +1,3 @@
+
+Pyr::Base::Engine.config.pyr_api_key = "Bullseye"
+Pyr::Base::Engine.config.pyr_api_secret = "RecruitMe"
