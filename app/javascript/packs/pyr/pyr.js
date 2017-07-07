fbbd469d1d0744b43851404ebd7174682efcf2f2
John Cwikla <cwikla@fwix.com>
2017-07-07 11:04:36 -0700

    Change HTTP_METHOD to Method

--- app/javascript/packs/pyr/pyr.js
+++ app/javascript/packs/pyr/pyr.js
@@ -8,11 +8,11 @@ const PUT = 'PUT';
 const GET = 'GET';
 const DELETE = 'DELETE';
 
-const HTTP_METHOD = { POST, PUT, GET, DELETE };
+const Method = { POST, PUT, GET, DELETE };
 
 const Pyr = { 
   Form, 
   Util,
-  HTTP_METHOD 
+  Method,
 };
 export default Pyr;
