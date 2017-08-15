4e9c3e91c363e0e879769c8093825a2429dfff69
John Cwikla <cwikla@fwix.com>
2017-08-15 15:01:45 -0700

    Scroll refactoring

--- app/javascript/packs/pages/messages/message_thread.jsx
+++ app/javascript/packs/pages/messages/message_thread.jsx
@@ -7,7 +7,14 @@ import ReactDOM from 'react-dom';
 import Pyr from '../../pyr/pyr';
 const ClassNames = Pyr.ClassNames;
 
-import ThreadItem from './thread_item';
+import ThreadItem, {
+  THREAD_ID
+} from './thread_item';
+
+const SCROLL_TIME = 800;
+
+const MESSAGES_URL = "/messages";
+
 
 const MessageThreadHeader = (props) => (
   <div className="message-header" {...Pyr.Util.propsRemove(props, ["job", "message", "isNew"])}>{props.isNew ? "New ": ""}Candidate: <span>{props.job.title}</span></div>
@@ -46,6 +53,60 @@ class ThreadList extends Component {
 }
 
 class Content extends Component {
+  constructor(props) {
+    super(props);
+
+    this.scroll = null;
+    this.onSetScroller = this.setScroller.bind(this);
+  }
+
+  setScroller(node) {
+    this.scroll = node;
+  }
+
+  firstUnread() {
+    let messages = this.props.thread;
+
+    let pos = messages.reduce((result, m, index) => {
+      if (m.mine || m.read_at) {
+        return index;
+      }
+      return result;
+    }, 0);
+    //console.log("READ POS: " + pos);
+    return messages[pos];
+  }
+
+  scrollToLastRead(firstUnread=false) {
+    console.log("SCROLL TO LAST");
+
+    if (!firstUnread) {
+      console.log("Scroll To Bottom");
+      //console.log("Scroll Height: " + this.scroll().scrollHeight);
+      this.scroll.scrollToBottom(SCROLL_TIME);
+      return;
+    }
+
+    let topMessage = this.firstUnread();
+    let mid = THREAD_ID(topMessage);
+
+    let $item = $("#" + mid);
+
+    console.log("SCROLLING TO ITEM: " + mid);
+    this.scroll.scrollToItem($item);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    let oldLength = prevProps.thread ? prevProps.thread.length : 0;
+    let newLength = this.props.thread ? this.props.thread.length : 0;
+
+    console.log("UPDATE: " + oldLength + " => " + newLength);
+
+    if (oldLength != newLength) {
+      this.scrollToLastRead(oldLength == 0);
+    }
+  }
+
   render() {
 
     if (!this.props.thread || this.props.thread.length < 1) {
@@ -55,6 +116,7 @@ class Content extends Component {
 
     return (
       <Pyr.Scroll
+        ref={(node) => this.scroll = node}
         id="message-show" 
         onScroll={this.props.onScroll}
       >
@@ -97,7 +159,83 @@ class Footer extends Component {
 }
 
 
-class MessageThread extends Component {
+class MessageThread extends Pyr.UserComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      thread: null
+    };
+
+    this.onLoading = null;
+    this.onSuccess = this.success.bind(this);
+  }
+
+  threadExtra(thread) {
+    let all = [];
+    let userId = this.context.user.id;
+
+    thread.map((item, pos) => {
+      item.is_root = !item.root_message_id;
+      item.mine = (item.from_user.id == userId);
+      item.read_at = new Date(); // got it so pretend it's been read, server is updating
+    });
+
+    return thread;
+  }
+
+  setThread(thread) {
+    this.setState({
+      thread: this.threadExtra(thread)
+    });
+  }
+
+  getThread(mid) {
+    let self =  this;
+
+    let url =  Pyr.URL(MESSAGES_URL).push(mid);
+
+    return Pyr.getJSON({
+      url: url,
+      context: self,
+      loading: self.onLoading
+    }).done(function(data, textStatus, jaXHR) {
+      self.setThread(data.thread);
+
+    }).fail(function(jaXHR, textStatus, errorThrown) {
+      Pyr.ajaxError(jaXHR, textStatus, errorThrown);
+    });
+  }
+
+  success(data, textStatus, jqXHR) {
+    let thread = this.state.thread || [];
+    thread = thread.concat(data.message);
+
+    console.log("THREAD LENGTH: " + thread.length);
+
+    this.setThread(thread);
+
+    //this.props.onSetItems(thread);
+  }
+
+
+  componentWillMount() {
+    let mid = this.props.message ? this.props.message.id : this.props.messageId;
+
+    if (mid) {
+      this.getThread(mid);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    let mid = this.props.message ? this.props.message.id : this.props.messageId;
+    let oid = prevProps.message ? prevProps.message.id : prevProps.messageId;
+
+    if (mid != oid) {
+      this.getMessage(mid);
+    }
+  }
+
   render() {
     return (
       <div className="sheet flx-col-stretch flx-1">
@@ -106,16 +244,17 @@ class MessageThread extends Component {
           onBack={this.props.onBack}
         />
         <Content
+          ref={(node) => this.content = (node)}
           onScroll={this.props.onScroll}
           message={this.props.message}
-          thread={this.props.thread}
+          thread={this.state.thread}
           jobMap={this.props.jobMap}
         >
         </Content>
       
         <Footer
           message={this.props.message}
-          onSuccess={this.props.onSuccess}
+          onSuccess={this.onSuccess}
           label="Reply"
           url={this.props.url}
         />
--- app/javascript/packs/pyr/scroll.jsx
+++ app/javascript/packs/pyr/scroll.jsx
@@ -11,6 +11,7 @@ class Scroll extends Component {
   constructor(props) {
     super(props);
 
+/*
     this.state = {
       top: 0,
       bottom: 0,
@@ -19,24 +20,63 @@ class Scroll extends Component {
       scrollHeight: 0,
       scrollWidth: 0
     };
+*/
 
     this.onScroll = this.scroll.bind(this);
   }
 
-  scrollTo(pos, animateTime=0) {
-    if (!animateTime) {
-      return $(this).stop().scrollTop(pos);
+  itemPos(item) {
+    let $item = $(item);
+    let $parent = $item.parent();
+
+    //console.log("ITEM: " + $item.attr("id"));
+    let top = $item.offset().top;
+
+    //console.log("TOP: " + $item.attr("id") + " => " + top);
+    //while($parent.attr("id") != "message-show") {
+    //while($parent.attr("id") != this.scroller.attr("id")) {
+    while(!$parent.is(this.scroller)) {
+      console.log("TOP: " + $parent.attr("id") + " => " + top);
+      top = top - $parent.offset().top;
+
+      $parent = $parent.parent();
+      //console.log("PARENT IS NOW: " + $parent.attr("id"));
     }
-    return $(this).stop().animate({
+
+    return top;
+  }
+
+  scrollToPos({pos, animateTime}) {
+    pos = pos || this.scroller[0].scrollHeight;
+    console.log("ScrollToPos: " + pos);
+    
+    if (!animateTime) {
+      return this.scroller.stop().scrollTop(pos);
+    }
+    return this.scroller.stop().animate({
       scrollTop: pos
     }, animateTime);
+  }
+
+  scrollToItem(item, animateTime) {
+    let pos = this.itemPos(item);
+    pos = pos + this.scroller.offset().top + $(item).height();
+    this.scrollToPos({pos, animateTime});
+  }
+
+  scrollToBottom(animateTime) {
+    this.scrollToPos({animateTime});
+  }
+
+  scrollToTop(animateTime) {
+    this.scrollToPos({pos: 0, animateTime});
   }
 
   scroll(e) {
     //console.log("A: " + this.scroller);
     //console.log("B: " + $(this.scroller).attr("id"));
 
-    let offset = $(this.scroller).offset();
+    let offset = this.scroller.offset();
     /*
     this.setState({
       top: offset.top,
@@ -54,10 +94,9 @@ class Scroll extends Component {
   }
 
   render() {
-        //onScroll={this.onScroll}
     return (
       <div 
-        ref={(node) => this.scroller = (node)}
+        ref={(node) => this.scroller = $(node)}
         {...Util.propsMergeClassName(this.props, "scroll flx-col flx-stretch")}
         onScroll={this.onScroll}
       >
