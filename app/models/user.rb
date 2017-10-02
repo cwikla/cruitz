f9cab88bc6a6c3d1e947a94b6b3ccdc8bae64aaf
John Cwikla <cwikla@fwix.com>
2017-10-02 14:33:55 -0700

    hack for duplicate emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,10 @@ class User < ApplicationRecord
 
   belongs_to :company
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true # FIXME, this isn't good
+
   def enough_info?
     return !self.first_name.blank? && !self.last_name.blank?
   end
