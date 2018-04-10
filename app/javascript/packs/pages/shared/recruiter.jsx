9284068d99ad93476d27d74517a33a1178a6051d
John Cwikla <cwikla@fwix.com>
2018-04-09 17:05:59 -0700

    Not bad.

--- app/javascript/packs/pages/shared/recruiter.jsx
+++ app/javascript/packs/pages/shared/recruiter.jsx
@@ -30,33 +30,38 @@ class Blurb extends Component {
     let rest = Pyr.Util.propsRemove(this.props, "recruiter");
 
     let inlineStyle = {
-/*
       backgroundImage: "url(" + getLogo(recruiter.id) + ")",
-      backgroundColor: "rgba(255,255,255,0.5)"
-*/
+      backgroundRepeat: "no-repeat",
+      backgroundAttachment: "local",
+      backgroundPosition: "center",
+      backgroundSize: "contain",
     };
 
  //   console.log(inlineStyle);
 
 
     return (
-      <div {...Pyr.Util.propsMergeClassName(rest, "recruiter-blurb")} style={inlineStyle}>
-        <div className="icon flx-row flx-start" >
-          <Pyr.UI.IconButton name="flag" className="spam"/>
-          <Pyr.UI.IconButton name="eye" className="ml-auto"/>
-        </div>
+      <div {...Pyr.Util.propsMergeClassName(rest, "recruiter-blurb ")} style={inlineStyle}>
+        <div className="rec-blurb-inner fifty">
+          <div>
+          <div className="icon flx-row flx-start" >
+            <Pyr.UI.IconButton name="flag" className="spam"/>
+            <Pyr.UI.IconButton name="eye" className="ml-auto"/>
+          </div>
 
-        <UserAvatar
-          userId={recruiter.id}
-          name={recruiter.first_name}
-        />
-        <Stars rating={3.7}/>
-        <div className="rate">64% acceptance rate</div>
-        <div className="company">Awesome Recruiting, Inc</div>
-        <div className="location">San Francisco, CA</div>
-        <div className="blurb">I specialize in recruiting software engineers
-        from top 10 schools.  I have worked with Uber, Twitter,
-        BobCo and SallyCo.</div>
+          <UserAvatar
+            userId={recruiter.id}
+            name={recruiter.first_name}
+          />
+          <Stars rating={3.7}/>
+          <div className="rate">64% acceptance rate</div>
+          <div className="company">Awesome Recruiting, Inc</div>
+          <div className="location">San Francisco, CA</div>
+          <div className="blurb">I specialize in recruiting software engineers
+          from top 10 schools.  I have worked with Uber, Twitter,
+          BobCo and SallyCo.</div>
+          </div>
+        </div>
       </div>
     );
   }
