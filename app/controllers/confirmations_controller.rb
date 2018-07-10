80fd8074ff1f1c025c812555c3bb2fdb0b36685f
John Cwikla <cwikla@fwix.com>
2018-07-09 22:49:35 -0700

    Dohp

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -5,7 +5,7 @@ class ConfirmationsController < Pyr::Base::ConfirmationsController
   CHECK_TIME = 60  * 30 # 30 minutes
 
   def after_confirmation_path_for(resource_name, resource)
-    sign_in(resource) if (Time.zone.now - resource.confirmed_at) > CHECK_TIME
+    sign_in(resource) if (Time.zone.now - resource.confirmed_at) <= CHECK_TIME
     if signed_in?(resource_name)
       signed_in_root_path(resource)
     else
