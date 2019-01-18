58885135e7b25b955d421f50ef1a85fcb6334ba4
John Cwikla <cwikla@fwix.com>
2019-01-18 13:09:42 -0800

    Remove all hardcoded access keys. And yes, all of them have been deactivated.

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,43 +1,14 @@
 require 'pusher'
 
-development = {
-  app_id: "541804",
-  key: "c9b6d0b4647c88393e62",
-  secret: "68afd2bfdfcf91ab1a35",
-  cluster: "us2",
-  encrypted: false
-}
-
-staging = {
-  app_id: '541805',
-  key: '95b89cfee87822b4ecbe',
-  secret: '5f68b0f6d2d499123a21',
-  cluster: 'us2',
-  encrypted: true
-}
-
-production = {
-  app_id: "541806",
-  key: "e9312cc8007eaaa1a9c1",
-  secret: "b9202f74d06dca616f58",
-  cluster: "us2"
-}
-
-pusher_args = {
-  development: development,
-  staging: staging,
-  production: production,
-}
-
 USE_PUSHER = ENV['USE_PUSHER'] || false
 puts "Pusher is on!" if USE_PUSHER
 
-PUSHER_APP_ID = pusher_args[Rails.env.to_sym][:app_id]
-PUSHER_KEY = pusher_args[Rails.env.to_sym][:key]
-PUSHER_SECRET = pusher_args[Rails.env.to_sym][:secret]
-PUSHER_CLUSTER = pusher_args[Rails.env.to_sym][:cluster]
-PUSHER_LOGGER = pusher_args[Rails.env.to_sym][:logger]
-PUSHER_ENCRYPTED = pusher_args[Rails.env.to_sym][:encrypted]
+PUSHER_APP_ID = ENV["PUSHER_APP_ID"]
+PUSHER_KEY = ENV["PUSHER_KEY"]
+PUSHER_SECRET = ENV["PUSHER_SECRET"]
+PUSHER_CLUSTER = ENV["PUSHER_CLUSTER"] || 'us2'
+PUSHER_LOGGER = ENV["PUSHER_LOGGER"]
+PUSHER_ENCRYPTED = ENV["PUSHER_ENCRYPTED"] || false
 
 Pusher.app_id = ::PUSHER_APP_ID
 Pusher.key = ::PUSHER_KEY
